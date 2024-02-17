d4486256df1bf2587e07f22a09f15bf6067d6da8
Hannes <157137227+HannesBrs@users.noreply.github.com>
2024-02-17 23:40:37 +0100

    FEAT-added comments to HelloController class

--- src/main/java/com/filmverleih/filmverleih/HelloController.java
+++ src/main/java/com/filmverleih/filmverleih/HelloController.java
@@ -3,6 +3,11 @@ package com.filmverleih.filmverleih;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * auto generated example Controller by JavaFX for a small HelloWorld application
+ * to test if the setup was successful
+ * @author auto generated
+ */
 public class HelloController {
     @FXML
     private Label welcomeText;
