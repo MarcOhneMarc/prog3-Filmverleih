285d0bedc7498bafa385fef20c01e278c93b826b
Hannes <157137227+HannesBrs@users.noreply.github.com>
2024-02-17 17:56:28 +0100

    FEAT-added SettingsController settings-view.fxml application.css logo.png (everything from "old project")

--- src/main/java/com/filmverleih/filmverleih/HelloApplication.java
+++ src/main/java/com/filmverleih/filmverleih/HelloApplication.java
@@ -2,20 +2,43 @@ package com.filmverleih.filmverleih;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("settings-view.fxml"));
+            Scene scene = new Scene(root);
+
+            Image icon = new Image(getClass().getResourceAsStream("logo.png"));
+            stage.getIcons().add(icon);
+
+            String css = this.getClass().getResource("application.css").toExternalForm();
+            scene.getStylesheets().add(css);
+
+            stage.setTitle("Quantum-Vortex");
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            //stage.setResizable(false);
+            stage.show();
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 
     public static void main(String[] args) {
         launch();
