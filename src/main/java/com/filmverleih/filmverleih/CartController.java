908db88fd57a0c75dd415e1a1f02854430476629
Hannes <157137227+HannesBrs@users.noreply.github.com>
2024-03-08 18:04:15 +0100

    FEAT-added some comments on CartController.java

--- src/main/java/com/filmverleih/filmverleih/CartController.java
+++ src/main/java/com/filmverleih/filmverleih/CartController.java
@@ -6,16 +6,18 @@ import javafx.scene.layout.BorderPane;
 
 public class CartController {
 
-    @FXML
-    private BorderPane bdp_CartBorderPane;
-
     NWayControllerConnector<NavbarController,LibraryController,MovieController,RentalController,SettingsController,FilterController,CartController, Integer,Integer,Integer> connector;
 
-
+    /**
+     * sets NWayControllerConnector as active connector for this controller, called from MainApplication
+     * @param connector the controller passed by MainApplication
+     */
     public void setConnector(NWayControllerConnector<NavbarController,LibraryController,MovieController,RentalController,SettingsController,FilterController,CartController, Integer,Integer,Integer> connector) {
         this.connector = connector;
     }
 
+    @FXML
+    private BorderPane bdp_CartBorderPane;
     @FXML
     private TextField txf_CartName;
     @FXML
@@ -33,11 +35,19 @@ public class CartController {
     @FXML
     private Button btn_OrderCart;
 
+    /**
+     * test method to link the order button to the controller
+     * which prints a small verification message in the console
+     * that the order button has been clicked
+     */
     @FXML
     public void orderCart() {
         System.out.println("console test: order button has been clicked");
     }
 
+    /**
+     * @return passes the main frame if the scene to the Controller it is called from
+     */
     public BorderPane getOuterPane() {
         return bdp_CartBorderPane;
     }
