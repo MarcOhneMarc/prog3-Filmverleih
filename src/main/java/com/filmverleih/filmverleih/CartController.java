9ea045003a2d298618cdc3bb1cab82e625eba7d0
Hannes <157137227+HannesBrs@users.noreply.github.com>
2024-03-08 18:57:40 +0100

    FEAT-test method in CartController to fill the tableView with temporary movie objects (inner class) + comments

--- src/main/java/com/filmverleih/filmverleih/CartController.java
+++ src/main/java/com/filmverleih/filmverleih/CartController.java
@@ -1,9 +1,27 @@
 package com.filmverleih.filmverleih;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.collections.ObservableList;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ * controller class for the cart frame of the application
+ * where the shopping cart can be seen with a list of
+ * the selected movies, providing a price calculation
+ * and displaying the rental and return date.
+ *
+ * TODO Movie Boxes on the left side of the splitPane
+ * TODO Price calculation
+ * TODO Date calculation
+ * TODO connect Backend
+ *
+ * @author Hannes, Jannis
+ */
 public class CartController {
 
     NWayControllerConnector<NavbarController,LibraryController,MovieController,RentalController,SettingsController,FilterController,CartController, Integer,Integer,Integer> connector;
@@ -25,7 +43,11 @@ public class CartController {
     @FXML
     private TextField txf_CartID;
     @FXML
-    private TableView tbv_CartItemsTable;
+    private TableView<FilmForTest> tbv_CartItemsTable;
+    @FXML
+    private TableColumn<FilmForTest, String> tbc_Movie;
+    @FXML
+    private TableColumn<FilmForTest, Double> tbc_Price;
     @FXML
     private Label lbl_CartTotalValue;
     @FXML
@@ -36,12 +58,59 @@ public class CartController {
     private Button btn_OrderCart;
 
     /**
+     *This inner Class is ONLY for Testing the Table view
+     * and creates simple Movie Objects with the Fields
+     * needed for the TableView Columns
+     * TODO Delete or update if Backend is connected
+     */
+    public static class FilmForTest {
+        private String title;
+        private double price;
+        public FilmForTest(String title, double price) {
+            this.title = title;
+            this.price = price;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+
+    /**
+     * This method creates a testList with FilmForTest Objects
+     * to fill the TableView
+     * TODO Delete of update if Backend is connected
+     */
+    public void fillTableView() {
+        ObservableList<FilmForTest> testList = FXCollections.observableArrayList();
+        for (int i = 0; i < 15; i++) {
+            testList.add(new FilmForTest("test" +i, 7.50));
+        }
+
+        tbv_CartItemsTable.setItems(testList);
+
+        tbc_Movie.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+        tbc_Price.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getPrice()).asObject());
+    }
+
+
+
+    /**
      * test method to link the order button to the controller
      * which prints a small verification message in the console
      * that the order button has been clicked
+     *
+     * Also added a call for the fillTableView() Method in order
+     * to test the TableView, after pressing the button once.
+     * TODO Delete of update if Backend is connected
      */
     @FXML
     public void orderCart() {
+        fillTableView();
         System.out.println("console test: order button has been clicked");
     }
 
