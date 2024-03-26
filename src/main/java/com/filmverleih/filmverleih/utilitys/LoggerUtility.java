8fae4025d61356fd81bef9dc6793e082c78e777d
Hannes <157137227+HannesBrs@users.noreply.github.com>
2024-03-26 13:45:15 +0100

    FEAT-added SLF4J and Log4J2 to pom.xml -added SLF4J to module-info.java -added LoggerUtility.java

---
+++ src/main/java/com/filmverleih/filmverleih/utilitys/LoggerUtility.java
@@ -0,0 +1,10 @@
+package com.filmverleih.filmverleih.utilitys;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtility {
+
+    public static final Logger logger = LoggerFactory.getLogger(LoggerUtility.class);
+
+}
