83d395b453bdff1e7d9165e003692b29c81a8132
Hannes <157137227+HannesBrs@users.noreply.github.com>
2024-03-26 15:34:17 +0100

    FEAT-added comments and changed prints to logger in old utility class

--- src/main/java/com/filmverleih/filmverleih/Utility.java
+++ src/main/java/com/filmverleih/filmverleih/Utility.java
@@ -4,6 +4,7 @@ import com.filmverleih.filmverleih.entity.Customers;
 import com.filmverleih.filmverleih.entity.Movies;
 import com.filmverleih.filmverleih.entity.Users;
 import com.filmverleih.filmverleih.entity.Rentals;
+import com.filmverleih.filmverleih.utilitys.LoggerUtility;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -46,10 +47,12 @@ public class Utility {
                 return movies;
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
+                //e.printStackTrace(); // replace with logger
+                LoggerUtility.logger.warn("old utility class");
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
+            //e.printStackTrace(); // replace with logger
+            LoggerUtility.logger.warn("old utility class");
         }
         return new ArrayList<Movies>();
     }
@@ -82,11 +85,14 @@ public class Utility {
                 transaction.commit();
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
+                //e.printStackTrace(); // replace with logger
+                LoggerUtility.logger.warn("old utility class");
+
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
+            //e.printStackTrace(); // replace with logger
+            LoggerUtility.logger.warn("old utility class");
             return false;
         }
         return true;
@@ -104,11 +110,15 @@ public class Utility {
                 transaction.commit();
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
+                //e.printStackTrace(); // replace with logger
+                LoggerUtility.logger.warn("old utility class");
+
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
+            //e.printStackTrace(); // replace with logger
+            LoggerUtility.logger.warn("old utility class");
+
             return false;
         }
         return true;
@@ -188,11 +198,15 @@ public class Utility {
 
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
+                //e.printStackTrace(); // replace with logger
+                LoggerUtility.logger.warn("old utility class");
+
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
+            //e.printStackTrace(); // replace with logger
+            LoggerUtility.logger.warn("old utility class");
+
             return false;
         }
         return true;
@@ -216,10 +230,14 @@ public class Utility {
                 transaction.commit();
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
+                //e.printStackTrace(); // replace with logger
+                LoggerUtility.logger.warn("old utility class");
+
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
+            //e.printStackTrace(); // replace with logger
+            LoggerUtility.logger.warn("old utility class");
+
         }
         return returnMovie;
     }
@@ -274,10 +292,14 @@ public class Utility {
                 return users;
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace();
+                //e.printStackTrace();
+                LoggerUtility.logger.warn("old utility class");
+
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            LoggerUtility.logger.warn("old utility class");
+
         }
         return  new ArrayList<Users>();
     }
@@ -297,10 +319,14 @@ public class Utility {
                 return customers;
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace();
+                //e.printStackTrace();
+                LoggerUtility.logger.warn("old utility class");
+
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            LoggerUtility.logger.warn("old utility class");
+
         }
         return  new ArrayList<Customers>();
     }
@@ -353,13 +379,17 @@ public class Utility {
                 transaction.commit();
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
-                System.out.println("customer registration went wrong Code: 77621");
+                //e.printStackTrace(); // replace with logger
+                //System.out.println("customer registration went wrong Code: 77621");
+                LoggerUtility.logger.warn("old utility class");
+
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
-            System.out.println("customer registration went wrong Code: 77620");
+            //e.printStackTrace(); // replace with logger
+            //System.out.println("customer registration went wrong Code: 77620");
+            LoggerUtility.logger.warn("old utility class");
+
             return false;
         }
         return true;
@@ -391,13 +421,17 @@ public class Utility {
                 transaction.commit();
             } catch (Exception e) {
                 if (transaction != null) transaction.rollback();
-                e.printStackTrace(); // replace with logger
-                System.out.println("Order went wrong Code: 77619");
+                //e.printStackTrace(); // replace with logger
+                //System.out.println("Order went wrong Code: 77619");
+                LoggerUtility.logger.warn("old utility class");
+
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace(); // replace with logger
-            System.out.println("Order went wrong Code: 77618");
+            //e.printStackTrace(); // replace with logger
+            //System.out.println("Order went wrong Code: 77618");
+            LoggerUtility.logger.warn("old utility class");
+
             return false;
         }
         return true;
--- src/main/java/com/filmverleih/filmverleih/utilitys/LoggerUtility.java
+++ src/main/java/com/filmverleih/filmverleih/utilitys/LoggerUtility.java
@@ -3,6 +3,10 @@ package com.filmverleih.filmverleih.utilitys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class provides a static Logger using SLF4J
+ * @author Hannes, Luka
+ */
 public class LoggerUtility {
 
     public static final Logger logger = LoggerFactory.getLogger(LoggerUtility.class);
